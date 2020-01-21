bed6bf3cf8ac94e525e6032a016e2aaee200f1d0
lawsonhung <lawsonhung@yahoo.com>
2020-01-21 17:22:55 -0500

    added test cases

--- Chapter 01 Arrays and Strings/1.02 Check Permutation/js/checkPermute.js
+++ Chapter 01 Arrays and Strings/1.02 Check Permutation/js/checkPermute.js
@@ -1,1 +1,15 @@
 console.log("You have connected to the matrix");
+console.log(true);
+console.log(false);
+
+const javaPairs = [["apple", "papel"], ["carrot", "tarroc"], ["hello", "llloh"], ["Hello", "Hello"]];
+const jsPairs = [["aba", "aab"], ["aba", "aaba"], ["aba", "aa"]];
+
+let checkPermute = (input1, input2) => {
+  console.log("The first string is: " + input1)
+  console.log("The second string is: " + input2)
+}
+
+for (let i = 0; i < javaPairs.length; i++){
+  
+};
