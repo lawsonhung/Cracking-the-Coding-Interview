4d9eb54ecfa90188b59738bb47d21fb01c2207a6
lawsonhung <lawsonhung@yahoo.com>
2020-05-07 21:07:57 -0400

    Define HashTable.set(key,value) Move package.json up 1 level in directory to parent directory so other directories can access and use modules in ES6 syntax

--- Chapter 01 Arrays and Strings/HashTable.js
+++ Chapter 01 Arrays and Strings/HashTable.js
@@ -22,6 +22,20 @@ class HashTable {
     const charCodeSum = charCodes.reduce((acc, cur) => acc + cur);
     return charCodeSum;
   }
+
+  // Add a key-value pair
+  set(key, value) {
+    // Hash the key
+    const hash = this.hash(key);
+
+    if (!this.data[hash]) {
+      this.data[hash] = [];
+    }
+
+    this.data[hash].push([key, value]);
+
+    this.size++;
+  }
 }
 
 //  console.log("hash('name): ", hash('name'));
@@ -29,4 +43,13 @@ class HashTable {
 
 const newHashTable = new HashTable();
 console.log("newHashTable: ", newHashTable);
-console.log("newHashTable.hash('name'): ", newHashTable.hash('name'));
+// console.log("newHashTable.hash('name'): ", newHashTable.hash('name'));
+
+newHashTable.set("name", "miku86");
+console.log(newHashTable.data);
+
+newHashTable.set("mean", false);
+console.log(newHashTable.data);
+
+newHashTable.set("age", 33);
+console.log(newHashTable.data);
